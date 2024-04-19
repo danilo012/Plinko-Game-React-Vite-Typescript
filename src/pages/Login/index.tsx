431a29444d4f0460c347bbca340dae2d738738a9
danilo012 <46241643+danilo012@users.noreply.github.com>
2024-04-19 14:48:49 +0800

    Update index.tsx

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -2,11 +2,7 @@ import { GoogleLogo } from 'phosphor-react'
 import { useEffect } from 'react'
 import { useLocation, useNavigate } from 'react-router-dom'
 import { useAuthStore } from 'store/auth' 
-import React, { useState } from "react";
-import Form from "react-bootstrap/Form";
-import Stack from "react-bootstrap/Stack";
-import Button from "react-bootstrap/Button";
-import "./Login.css";
+
 
 type LocationState = {
   from?: string
@@ -46,46 +42,3 @@ export function LoginPage() {
     </div>
   )
 }
-export default function Login() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-
-  function validateForm() {
-    return email.length > 0 && password.length > 0;
-  }
-
-  function handleSignIn(event: React.FormEvent<HTMLFormElement>) {
-    event.preventDefault();
-  }
-
-  return (
-    <div className="Login">
-      <Form onSubmit={handleSignIn}>
-        <Stack gap={3}>
-          <Form.Group controlId="email">
-            <Form.Label>Email</Form.Label>
-            <Form.Control
-              autoFocus
-              size="lg"
-              type="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-            />
-          </Form.Group>
-          <Form.Group controlId="password">
-            <Form.Label>Password</Form.Label>
-            <Form.Control
-              size="lg"
-              type="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
-          </Form.Group>
-          <Button size="lg" type="SignIn" disabled={!validateForm()}>
-            Login
-          </Button>
-        </Stack>
-      </Form>
-    </div>
-  );
-        }
